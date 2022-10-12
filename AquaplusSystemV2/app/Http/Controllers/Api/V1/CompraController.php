9a6fece9292f6a6dceac2b1db42daaa8c5f9a0c8
Darklord S.Shadow <joradrian2017@gmail.com>
2022-10-11 22:35:26 -0500

    Listar compras no estaba trayendo las ultimas 100 compras, arreglado

--- AquaplusSystemV2/app/Http/Controllers/Api/V1/CompraController.php
+++ AquaplusSystemV2/app/Http/Controllers/Api/V1/CompraController.php
@@ -22,7 +22,7 @@ class CompraController extends Controller
     public function listarCompras()
     {
         try {
-            $compras = DB::select('Select c.*,p.nombre as proveedor, u.nombre as usuario from compras c inner join proveedores p on c.proveedor_id = p.id inner join usuarios u on c.usuario_id = u.id');
+            $compras = DB::select('Select  top 100 c.*,p.nombre as proveedor, u.nombre as usuario from compras c inner join proveedores p on c.proveedor_id = p.id inner join usuarios u on c.usuario_id = u.id');
             return response()->json(['data' => $compras, 'status' => 'true'], 200);
         } catch (\Exception $e) {
             return response()->json(['data' => $e->getMessage(), 'status' => 'false'], 500);
@@ -299,9 +299,7 @@ class CompraController extends Controller
     public function buscarCompras($proveedor_id = null)
     {
         if ($proveedor_id == null) {
-                return $this->listarCompras();
-            
-           
+                return $this->listarCompras();                       
         } else {
             $compras = DB::select('Select c.*,p.nombre as proveedor, u.nombre as usuario from compras c inner join proveedores p on c.proveedor_id = p.id inner join usuarios u on c.usuario_id = u.id where c.proveedor_id = ?', [$proveedor_id]);
             return response()->json(['data' => $compras, 'status' => 'true'], 200);
